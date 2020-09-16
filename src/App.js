e2c9a8ee92eefab9b391c640184816c364cd495c
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-09-16 22:19:13 +0200

    cosmetic changes in App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class BooksApp extends React.Component {
     sections: [
         {name: 'Currently Reading', sectionKey:'currentlyReading'},
         {name: 'Want to read', sectionKey:'wantToRead'},
-        {name: 'Read', sectionKey:'read'}],
+        {name: 'Read', sectionKey:'read'}
+    ],
 
     allBooks: [
         {url: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api", title: "To Kill a Mockingbird", author: "Harper Lee", status:"currentlyReading"},
